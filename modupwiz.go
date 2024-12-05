cb41805e9dc0726f542e2859d177124e06852fdd
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-05 01:08:19 +0100

    refactor: isolate render

--- modupwiz.go
+++ modupwiz.go
@@ -9,7 +9,6 @@ import (
 	"slices"
 
 	"github.com/ldez/modupwiz/internal"
-	"github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/mod/modfile"
 )
@@ -170,54 +169,4 @@ func filter(updates []internal.ModulePublic, opts Options, file *modfile.File) [
 	}
 
 	return modules
-}
-
-func render(opts Options, modules []internal.ModulePublic) error {
-	if len(modules) == 0 {
-		log.Println("No updates available. " + opts.scope())
-		return nil
-	}
-
-	writer := os.Stdout
-	if opts.Path != "" {
-		var err error
-		writer, err = os.Create(opts.Path)
-		if err != nil {
-			return fmt.Errorf("create file %s: %w", opts.Path, err)
-		}
-
-		defer func() { _ = writer.Close() }()
-	}
-
-	table := tablewriter.NewWriter(writer)
-
-	titles := []string{"Module"}
-	if opts.Versions {
-		titles = append(titles, "Current", "Latest")
-	}
-	if opts.Compare {
-		titles = append(titles, "Compare")
-	}
-
-	table.SetHeader(titles)
-	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
-	table.SetCenterSeparator("|")
-
-	for _, module := range modules {
-		row := []string{module.Path}
-
-		if opts.Versions {
-			row = append(row, module.Version, module.NewVersion())
-		}
-
-		if opts.Compare {
-			row = append(row, getCompareLink(module))
-		}
-
-		table.Append(row)
-	}
-
-	table.Render()
-
-	return nil
 }
